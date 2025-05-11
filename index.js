977f14e673ac5da971794d215f35529c819eef42
matioonet <Matinyonesii@gmail.com>
2025-05-12 02:43:09 +0330

    Update index.js

--- index.js
+++ index.js
@@ -133,3 +133,14 @@ bot.on('text', async (ctx) => {
 });
 
 bot.launch();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(port, () => {
+  console.log(`Web server is running on port ${port}`);
+});
